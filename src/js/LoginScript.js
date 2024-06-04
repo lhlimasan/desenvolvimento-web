d80ec4040e581b327c16df4e7569fa37a5ceed0b
Wescley Silva <wescley.silva.cas@gmail.com>
2024-06-03 23:28:16 -0300

    [FTR] :sparkles: Login e Cadastro de usuarios

---
+++ src/js/LoginScript.js
@@ -0,0 +1,37 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    // Validações
+    if (email === "") {
+        alert('E-mail não pode ser vazio.');
+        return;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert('Por favor, insira um e-mail válido. O e-mail deve ter o formato "xxx@xxx.com".');
+        return;
+    }
+
+    if (password.length < 8) {
+        alert('A senha deve ter no mínimo 8 caracteres.');
+        return;
+    }
+
+    axios.post('http://api.wescleydev.com/api/login', { email, password })
+        .then(response => {
+            const { token } = response.data;
+            localStorage.setItem('authToken', token);
+            console.log(token)
+
+            alert('Login realizado com sucesso!');
+            window.location.href = 'index.html';
+        })
+        .catch(error => {
+            console.error('Erro ao fazer login:', error.response ? error.response.data : error.message);
+            alert('Erro ao fazer login. Verifique suas credenciais e tente novamente.');
+        });
+});
---
+++ src/js/RegisterScript.js
@@ -0,0 +1,37 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    // Validações
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === "" || !emailPattern.test(email)) {
+        alert('Por favor, insira um e-mail válido. O e-mail deve ter o formato "xxx@xxx.com".');
+        return;
+    }
+
+    if (password.length < 8) {
+        alert('A senha deve ter no mínimo 8 caracteres.');
+        return;
+    }
+
+    const user = {
+        name: name,
+        email: email,
+        password: password
+    };
+
+    axios.post('http://api.wescleydev.com/api/users', user)
+        .then(response => {
+            alert('Usuário cadastrado com sucesso!');
+            window.location.href = '/login.html';
+            document.getElementById('registerForm').reset();
+
+        })
+        .catch(error => {
+            console.error('Erro ao cadastrar usuário:', error.response ? error.response.data : error.message);
+            alert('Erro ao cadastrar usuário. Tente novamente.');
+        });
+});
